a76b208ed1b80a653cd0177cb7b35bd8aa8a9e20
Hypercore <hypercore@me.com>
2013-11-09 19:45:27 +0800

    Now you're able to recite reversely or randomly.

--- reciter.py
+++ reciter.py
@@ -1,34 +1,41 @@
 # coding=utf-8
 import argparse
+import random
 
 t = 0
-d = {}
+d = []
 succ = False
 ans = ''
 
-# read the arguments and parse them into a dictionary
+# read the arguments and parse them into a 2D list
 parser = argparse.ArgumentParser(description='Simple dictation tool.')
-parser.add_argument('file', help='the dictionary you want to recite.', action='store', nargs=1, type=file)
-parser.add_argument('-r', '--reverse', help='reversely recite.', action='store_true')
+parser.add_argument('file', help='the dictionary you want to recite', action='store', nargs=1, type=file)
+parser.add_argument('-r', '--reverse', help='reversely recite(ignored when using with "-R")', action='store_true')
+parser.add_argument('-R', '--random', help='randomly recite', action='store_true')
 args=parser.parse_args()
 for k in args.file[0].read().strip().split('\n'):
-    j = k.split()
-    d[j[0]] = j[1]
-# print d
+     d.append(k.split())
 
+# reverse or randomly the 2D list(if needed)
+if args.reverse and not args.random:
+    d.reverse()
+elif args.random:
+    random.shuffle(d)
+
+print(args)
 # main loop
 while(not succ):
     succ = True
     print('\nTurn %s' % (t + 1))
     for k in d:
-        if d[k] != True:
-            ans = d[k]
-            if raw_input('%s:' % k) == ans:
-                d[k] = True
+        if k[1] != True:
+            ans = k[1]
+            if raw_input('%s:' % k[0]) == ans:
+                k[1] = True
             else:
                 print("^\n%s" % ans)
         for j in d:
-            if d[j] != True:
+            if j[1] != True:
                 succ = False
                 break
     if succ:
